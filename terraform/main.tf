1a4a2bbc158d4cea63b3172839d492bc16fe5084
Roger Amorim <rogertc16@gmail.com>
2022-11-16 23:44:13 -0300

    second commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,12 @@ resource "aws_instance" "app_server" {
     Name = "ExampleInstance"
   }
 }
+
+resource "aws_instance" "app_server_recovery" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstanceRecovery"
+  }
+}
